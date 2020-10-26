a6b8cf70c7be93b62d43bd32668f89c26acb8f63
Ian Hart <ianhart7790@gmail.com>
2020-10-26 17:20:45 -0400

    configuring aws

--- public/classes/Task.php
+++ public/classes/Task.php
@@ -71,7 +71,7 @@ class Task
       'Bucket' => 'hart-taskify',
       'Key' => $key,
       'ACL'    => 'public-read',
-      'ContentType' => explode('.', $key)[1]
+      'ResponseContentType' => explode('.', $key)[1]
     ]);
 
     return $result->get('ObjectURL');
